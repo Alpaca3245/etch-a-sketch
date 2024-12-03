08e0471e41f6b8784dbd3c1ae42885eb3f65ddf0
Alpaca3245 <kyle.luu11@gmail.com>
2024-12-03 16:39:15 -0500

    Created createGridElem() and createGrid() which appends a grid to the webpage

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-// Create a grid row with number of square assigned to MAXGRIDSQUARES
-function createGridRow() {
-    const gridSquare = document.createElement("div");
+// Create a grid row element with number of square assigned to MAXGRIDSQUARES
+function createGridRowElem(MAXGRIDSQUARES) {
+    
     const gridRow = document.createElement("div");
-    const MAXGRIDSQUARES = 16;
-
-    gridSquare.className = "gridSquare";
     gridRow.className = "gridRow";
 
     // Appends 16 squares to grid row
     for (let i = 0; i < MAXGRIDSQUARES; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.className = "gridSquare";
         gridRow.appendChild(gridSquare);
     }
+    return gridRow;
 }
 
+function createGridElem() {
+    const MAXROWS = 16;
+    const MAXGRIDSQUARES = 16;
 
+    const grid = document.createElement("div");
+    grid.className=("grid")
+    
+    for (let i = 0; i < MAXROWS; i++) {
+        const gridRow = createGridRowElem(MAXGRIDSQUARES);
+        grid.appendChild(gridRow);
+    }
+
+    return grid;
+}
+
+// Appends grid to etch-a-sketch-container
+function createGrid() {
+    const etchASketchContainer = document.getElementById("etch-a-sketch-container");
+    etchASketchContainer.appendChild(createGridElem());
+}
+
+createGrid();
